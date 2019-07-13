bcc9bac0440a69fdfcb09f6f116cf589fd6ba3b2
coolsidd <sidharth_0903@hotmail.com>
2019-07-12 22:39:49 -0400

    Final Changes at HBCSE

--- build_iso.sh
+++ build_iso.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ./simple-cdd
+mkdir -p ./local_extras/
 git archive -o ./local_extras/DOER_OS.zip HEAD --format=zip
 build-simple-cdd -p test --keyring ../new-debian-archive-keyring.gpg --conf ./test.conf
